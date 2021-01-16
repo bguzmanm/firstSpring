c2afd07de5d9f77f509a5ba28ba79a578133bee0
Brian <brian.guzman@gmail.com>
2021-01-15 21:13:24 -0300

    second commit

--- src/main/java/cl/awakelab/spring/MainController.java
+++ src/main/java/cl/awakelab/spring/MainController.java
@@ -1,15 +1,34 @@
 package cl.awakelab.spring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import cl.awakelab.model.Estudiante;
 
 @Controller
 public class MainController {
 
 	@RequestMapping(value="/", method = RequestMethod.GET)
-	public String ppal() {
+	public String ppal(Model model) {
+		
+		//armo lista de datos que voy a mostrar.
+		List<Estudiante> lista = new ArrayList<Estudiante>();
+		lista.add(new Estudiante(0, "Yossie"));
+		lista.add(new Estudiante(1, "Javiera"));
+		lista.add(new Estudiante(2, "Gustavo"));
+		
+		//y la paso como atributo de model
+		model.addAttribute("lista", lista);
+		
 		return "index";
+		
+		
+		
 	}
 	
 	@RequestMapping(value="/Caracteristicas", method=RequestMethod.GET)
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,3 +1,4 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
 <!DOCTYPE html>
@@ -15,6 +16,23 @@
 	<div class="container">
 		<h1>HOME</h1>
 		<%@include file="menu.jsp" %>
+		
+		<table class="table">
+			<thead class="table-dark">
+				<tr>
+					<th scope="col">#</th>
+					<th scope="col">Nombre</th>
+				</tr>
+			</thead>
+			<tbody>
+				<c:forEach var="e" items="${lista}">
+					<tr>
+						<td><c:out value="${e.getId()}"></c:out></td>
+						<td><c:out value="${e.getNombre()}"></c:out></td>
+					</tr>
+				</c:forEach>
+			</tbody>
+		</table>
 	</div>
 </body>
 </html>
